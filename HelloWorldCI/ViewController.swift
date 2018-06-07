df488ce2141352d10b53e1e97b4d9f7093c091b1
Paweena U <little_kam_00@hotmail.com>
2018-06-07 16:48:24 +0700

    init

---
+++ HelloWorldCI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldCI
+//
+//  Created by Kmm on 6/7/2561BE.
+//  Copyright © 2561 Kmm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
